b59859121c2573bbc4e30ea4bf739e0cc8f03781
Angshu Das <angshudas191@gmail.com>
2024-01-19 15:36:29 +0530

    added 2 math problems

--- MathProblems/FindAllThePrimeNumbersLessThanN.java
+++ MathProblems/FindAllThePrimeNumbersLessThanN.java
@@ -3,26 +3,29 @@ package revise_DSA.MathProblems;
 public class FindAllThePrimeNumbersLessThanN {
     public static void main(String[] args) {
         int n = 40;
-        for(int i=1; i<n; i++){
-            if(isPrime(i)){
-                System.out.println(i);
-            }
-        }
+        boolean[] arr = new boolean[n+1];
+        printPrimes(n, arr);
     }
 
-    static boolean isPrime(int n){
+    static void printPrimes(int n, boolean[] arr){
         if(n<=1){
-            return false;
+            System.out.println(-1);
         }
 
-        int c = 2;
-
-        while(c*c <= n){
-            if(n%c == 0){
-                return false;
+        for(int i=2; i*i <= n; i++){
+            if(!arr[i]){
+                for(int j=i*2; j<=n; j=j+i){
+                    arr[j] = true;
+                }
             }
-            c++;
         }
-        return true;
+
+        for(int i=2; i<=n; i++){
+            if(!arr[i]){
+                System.out.println(i+ " ");
+            }
+        }
     }
+
+
 }
