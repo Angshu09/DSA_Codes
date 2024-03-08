63e68cd7d0785f2d76a0824d843af614048788d5
Angshu Das <angshudas191@gmail.com>
2024-03-08 16:22:46 +0530

    modified

--- Recursion/Backtracking/Maze.java
+++ Recursion/Backtracking/Maze.java
@@ -1,9 +1,26 @@
 package revise_DSA.Recursion.Backtracking;
 
+import java.util.Arrays;
+
 public class Maze {
     public static void main(String[] args) {
-        System.out.println(countRoutes(3, 3));
-        printPaths("", 3, 3);
+//
+        boolean[][] maze = {
+                {true, true, true},
+                {true, false, true},
+                {true, true, true}
+        };
+//        mazeWithRestrictions("", maze, 0, 0);
+
+        boolean[][] maze2 = {
+                {true, true, true},
+                {true, true, true},
+                {true, true, true}
+        };
+//        mazeWithBackTracking("", maze2, 0, 0);
+
+        int[][] path = new int[maze2.length][maze2[0].length];
+        mazeWithBackTrackingPath("", maze2, 0, 0, path, 1);
     }
 
     static int countRoutes(int r, int c){
@@ -30,5 +47,93 @@ public class Maze {
         if(c>1){
             printPaths(p+'R', r, c-1);
         }
+    }
+
+    static void mazeWithRestrictions(String str, boolean[][] maze, int r, int c){
+        if(r==maze.length-1 && c==maze[0].length-1){
+            System.out.println(str);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        if(r < maze.length-1){
+            mazeWithRestrictions(str+'D', maze, r+1, c);
+        }
+
+        if(c< maze[0].length-1){
+            mazeWithRestrictions(str+'R', maze, r, c+1);
+        }
+    }
+
+    static void mazeWithBackTracking(String str, boolean[][] maze, int r, int c){
+        if(r==maze.length-1 && c==maze[0].length-1){
+            System.out.println(str);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        maze[r][c] = false;
+
+        if(r < maze.length-1){
+            mazeWithBackTracking(str+'D', maze, r+1, c);
+        }
+
+        if(c< maze[0].length-1){
+            mazeWithBackTracking(str+'R', maze, r, c+1);
+        }
+
+        if(r>0){
+            mazeWithBackTracking(str+'U', maze, r-1, c);
+        }
+
+        if(c>0){
+            mazeWithBackTracking(str+'L', maze, r, c-1);
+        }
+
+        maze[r][c] = true;
+    }
+
+    static void mazeWithBackTrackingPath(String str, boolean[][] maze, int r, int c, int[][] path, int step){
+        if(r==maze.length-1 && c==maze[0].length-1){
+            path[r][c] = step;
+            for(int[] arr: path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(str);
+            System.out.println();
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        maze[r][c] = false;
+        path[r][c] = step;
+
+        if(r < maze.length-1){
+            mazeWithBackTrackingPath(str+'D', maze, r+1, c, path, step+1);
+        }
+
+        if(c< maze[0].length-1){
+            mazeWithBackTrackingPath(str+'R', maze, r, c+1, path, step+1);
+        }
+
+        if(r>0){
+            mazeWithBackTrackingPath(str+'U', maze, r-1, c, path, step+1);
+        }
+
+        if(c>0){
+            mazeWithBackTrackingPath(str+'L', maze, r, c-1, path, step+1);
+        }
+
+        maze[r][c] = true;
+        step = 0;
     }
 }
