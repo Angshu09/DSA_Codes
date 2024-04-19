8593985ff791fd3e187652ba58ec20679936e018
Angshu Das <angshudas191@gmail.com>
2024-04-19 11:18:58 +0530

    added Method2 and modified

--- Stack/Questions/MinStack.java
+++ Stack/Questions/MinStack.java
@@ -42,6 +42,61 @@ package revise_DSA.Stack.Questions;
     }
 }*/
 
+//Method 2
+/* MinStack {
+
+    Stack<Long> st = new Stack<>();
+    long min = -1;
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        if(st.isEmpty()){
+            st.push((long)val);
+            min = (long)val;
+        }
+        else if(val >= min){
+            st.push((long)(val));
+        }
+        else{
+            st.push(2*(long)(val)-min);
+            min = (long)val;
+        }
+    }
+
+    public void pop() {
+        if(st.isEmpty()){
+            return;
+        }
+        else if(st.peek() > min){
+            st.pop();
+        }
+        else{
+            long old = 2*min - st.peek();
+            min = old;
+            st.pop();
+        }
+    }
+
+    public int top() {
+        long high = st.peek();
+        if(st.isEmpty()){
+            return -1;
+        }
+        else if(st.peek() > min){
+            return (int)high;
+        }
+        else{
+            return (int)min;
+        }
+    }
+
+    public int getMin() {
+        return (int)min;
+    }
+}*/
 
 public class MinStack {
 }
