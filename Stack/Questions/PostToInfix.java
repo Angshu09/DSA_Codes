142324dda27143fe32deb9020ed9a07dce1287ca
Angshu Das <angshudas191@gmail.com>
2024-04-30 15:17:00 +0530

    added

--- Stack/Questions/PostToInfix.java
+++ Stack/Questions/PostToInfix.java
@@ -1,2 +1,29 @@
-package revise_DSA.Stack.Questions;public class PostToInfix {
+package revise_DSA.Stack.Questions;
+
+import java.util.Stack;
+
+public class PostToInfix {
+    public static void main(String[] args) {
+        String str = "953+4*6/-";
+        Stack<String> st = new Stack<>();
+        String val1, val2;
+
+        // Iterating in default direction
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+
+            if((int)ch >= 48 && (int)ch <=57){
+                String s = "" + ch;
+                st.push(s);
+            }
+            else{
+                val2 = st.pop();
+                val1 = st.pop();
+                String ans = "(" + val1 + ch + val2 + ")";
+                st.push(ans);
+            }
+        }
+
+        System.out.println(st.peek());
+    }
 }
--- Stack/Questions/SlidingWindowMaximum.java
+++ Stack/Questions/SlidingWindowMaximum.java
@@ -1,2 +1,50 @@
-package revise_DSA.Stack.Questions;public class SlidingWindowMaximum {
+package revise_DSA.Stack.Questions;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class SlidingWindowMaximum {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, -1, -3, 5, 3, 6, 7};
+        System.out.println(Arrays.toString(maxSlidingWindow(arr, 3)));
+    }
+
+    private static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int z = 0;
+        Stack<Integer> st = new Stack<>();
+        int[] nge = new int[n];
+        int[] ans = new int[n-k+1];
+
+        nge[n-1] = n;
+        st.push(n-1);
+
+        // Finding the next greater element
+        for(int i=n-2; i>=0; i--){
+            while(st.size() > 0 && nums[i] > nums[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()) nge[i] = n;
+            else nge[i] = st.peek();
+            st.push(i);
+        }
+
+        int j = 0;
+        for(int i=0; i<n-k+1; i++){
+            if(j>=i+k){
+                j = i;
+            }
+            int max = nums[i];
+
+            while(j<i+k){
+                max = nums[j];
+                j = nge[j];
+            }
+
+            ans[z++] = max;
+        }
+
+        return ans;
+    }
 }
