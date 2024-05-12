883cc77038ed9026728053d960f8ae5299023b24
Angshu Das <angshudas191@gmail.com>
2024-05-12 17:34:23 +0530

    modified

--- Stack/Questions/MaximizeHappiness.java
+++ Stack/Questions/MaximizeHappiness.java
@@ -1,2 +1,28 @@
-package revise_DSA.Stack.Questions;public class MaximizeHappiness {
+package revise_DSA.Stack.Questions;
+
+//https://leetcode.com/problems/maximize-happiness-of-selected-children/description/
+
+public class MaximizeHappiness {
+//    public long maximumHappinessSum(int[] happiness, int k) {
+//        long sum = 0;
+//        Arrays.sort(happiness);
+//        Stack<Integer> st = new Stack<>();
+//
+//        for(int num: happiness){
+//            st.push(num);
+//        }
+//
+//        int i=0;
+//
+//        while(k!=0 && !st.isEmpty()){
+//            int temp = st.pop();
+//            if((temp-i) > 0){
+//                sum += (temp-i);
+//            }
+//            i++;
+//            k--;
+//        }
+//
+//        return sum;
+//    }
 }
